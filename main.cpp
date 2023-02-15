fa222c6080140089bdd9e362064fccedbf8050ea
David Wagih <davidwagih62@gmail.com>
2023-02-15 19:34:02 +0200

    implement a function to get the average of numbers

--- main.cpp
+++ main.cpp
@@ -13,10 +13,24 @@ int getSum(vector<int> v)
     return sum;
 }
 
+double getAverage(vector<int> nums)
+{
+    if (nums.empty())
+    {
+        return 0;
+    }
+    int sum = getSum(nums);
+    return static_cast<double>(sum) / nums.size();
+}
+
 int main()
 {
     vector<int> myVector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(myVector);
-    cout << "The sum of the elements in the vector is: " << sum << endl;
+    cout << "Sum: " << sum << endl;
+
+    double average = getAverage(myVector);
+    cout << "Average: " << average << endl;
+
     return 0;
 }
